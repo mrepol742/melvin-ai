ef0e316cf3141254d3565d72a78dd74e76e1b744
Melvin Jones Repol <mrepol742@gmail.com>
2026-05-01 17:04:40 +0800

    feat: initialize capacitor for android

---
+++ android/app/src/main/java/com/mrepol742/melvinai/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mrepol742.melvinai;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mrepol742.melvinai',
+  appName: 'melvin-ai',
+  webDir: 'out'
+};
+
+export default config;
